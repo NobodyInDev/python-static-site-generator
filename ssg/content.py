ccc5254cf54519286f3d324318ecd9d817d67f7c
Carl Hansen <carl.hansen@nzx.com>
2022-06-01 09:56:37 +1200

    Fixed bugs I'd previously missed

--- ssg/content.py
+++ ssg/content.py
@@ -6,13 +6,13 @@ from yaml import load
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimiter = "^(?:-|\+){3}\s*$"
+    __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter,re.MULTILINE)
 
     @classmethod
     def load(cls,string):
         _, fm, content = cls.__regex.split(string, 2)
-        load(fm, Loader = FullLoader)
+        metadata = load(fm, Loader = FullLoader)
         return cls(metadata, content)
 
     def __init__(self, metadata, content):
@@ -43,6 +43,6 @@ class Content(Mapping):
     def __repr__(self):
         data = {}
         for key, value in self.data.items():
-            if key != "content":
+            if (key != "content"):
                 data[key] = value
         return str(data)
