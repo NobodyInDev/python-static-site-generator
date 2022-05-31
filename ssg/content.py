fc13dd55a48c1d943107ce067a8fdf84e8ee8b72
Carl Hansen <carl.hansen@nzx.com>
2022-06-01 09:51:46 +1200

    fixed a minor bug

--- ssg/content.py
+++ ssg/content.py
@@ -9,6 +9,7 @@ class Content(Mapping):
     __delimiter = "^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter,re.MULTILINE)
 
+    @classmethod
     def load(cls,string):
         _, fm, content = cls.__regex.split(string, 2)
         load(fm, Loader = FullLoader)
