1ec107d4e4cfebc02b43108f35905bbaefbf1d58
Carl Hansen <carl.hansen@nzx.com>
2022-05-31 14:28:45 +1200

    Fixed compiler errors, code doesn't pass tests though

--- ssg/site.py
+++ ssg/site.py
@@ -7,11 +7,11 @@ class Site:
         self.source = Path(source)
         self.dest = Path(dest)
 
-    def create_dir(self):
+    def create_dir(self,path):
         part1 = self.dest
-        part2 = Path.relative_to(self.source)
-        directory = part1 + "/" + part2
-        Path(directory).mkdir(parents=True, exist_ok=True)
+        part2 = Path(path).relative_to(self.source)
+        directory = Path(part1,"/",part2)
+        directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
