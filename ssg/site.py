30d7a9b9f66720eb09dac0c84d8097645ff798a3
Carl Hansen <carl.hansen@nzx.com>
2022-05-31 17:02:38 +1200

    Maybe I fixed it this time

--- ssg/site.py
+++ ssg/site.py
@@ -6,7 +6,7 @@ class Site:
     def __init__(self, source, dest,parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
-        self.parsers = parsers or None
+        self.parsers = parsers or []
 
     def create_dir(self,path):
         directory = self.dest / path.relative_to(self.source)
@@ -17,7 +17,7 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-            elif(path.is_file):
+            elif(path.is_file()):
                 self.run_parser(path)
 
     def load_parser(self,extension):
