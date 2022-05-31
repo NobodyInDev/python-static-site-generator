651acf105d888bc1aac98fa967418667eaafa1bf
Carl Hansen <carl.hansen@nzx.com>
2022-06-01 10:17:26 +1200

    Fixed minor bugs

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -39,7 +39,7 @@ class MarkdownParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        self.write(html, path, dest)
+        self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 class ReStructuredTextParser(Parser):
@@ -47,5 +47,6 @@ class ReStructuredTextParser(Parser):
 
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
-        publish_parts(content.body, writer_name = "html5")
+        html = publish_parts(content.body, writer_name = "html5")
+        self.write(path, dest, html["html_body"])
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name,content))
